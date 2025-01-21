6f9e94e775765421f31123a1a63b156fff9e307b
Emilis01 <emilisdov@yahoo.com>
2025-01-21 17:36:05 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = 8080;
+
+app.use(cors());
+// KOMENTARASss
+// asdasdasdasdasd
+app.get('/', (req, res) => res.send(`Hello World from PagaliauSSS kazkas pavyko! Versija 7`));
+app.get('/user', (req, res) => res.send({
+	name: "petras",
+	email: "petras@gmail.com",
+	password: "123456",
+}));
+app.post('/user', (req, res) => {
+	const users = req.body;
+
+	console.log('users req.body', req.body);
+
+	res.send({
+		message: 'New user was added to the list',
+	});
+});
+
+app.listen(port);
+console.log(`App running on http://localhost:${port}`);
